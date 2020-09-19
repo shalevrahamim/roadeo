c38b61e0f32350b7bee85363577f7629e600d65e
user.name <zohar.yzgaev@healthy.io>
2020-09-20 00:21:10 +0300

    Skeleton for applicaton entrypoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const startServer = async () => {
+  const port = process.env.PORT || 1337;
+  return port;
+};
+
+module.exports = () => {
+  startServer()
+    .then((port) => {
+      const server = require('./app');
+      server().listen(port, () => {
+        console.log(`API is live and running on port: ${port}`);
+      });
+    })
+    .catch((error) => {
+      console.error('failed to start server', error);
+      throw error;
+    });
+};
