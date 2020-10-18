5306761dc91bcd89a7bfc1740bea960946ca14b8
user.name <zohar.yzgaev@healthy.io>
2020-10-18 14:32:07 +0300

    Introduce services

--- services/client-api/app.js
+++ services/client-api/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const { postgraphile } = require('postgraphile');
+const { getConnectionString } = require('../../infrastructure/db');
 const setupRoutes = require('./routes');
 
 const app = express();
+
+app.use(
+  postgraphile(process.env.DATABASE_URL || getConnectionString(), {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+  })
+);
 
 module.exports = () => {
   setupRoutes(app);
